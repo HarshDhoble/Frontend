0f5358eefc9434074373187444ee3f274770993a
HarshDhoble <harshdhoble26@gmail.com>
2021-07-30 19:35:17 +0530

    Tha23 Complete

---
+++ Day23/customhooks/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import DocTitleOne from './components/DocTitleOne';
+function App() {
+  return (
+    <div className="App">
+    <div className="header">Changing title live using Custom Hooks</div>
+    <DocTitleOne />
+    </div>
+  );
+}
+export default App;
